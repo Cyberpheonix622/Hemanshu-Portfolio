ce30efdd48553497ae2280d514abfe96eda84ef5
Creepboi <cyberpheonix622@gmail.com>
2025-07-16 23:47:57 +0530

    ok first commit

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+
+import Nav from "./Nav";
+
+const header = () => {
+  return (
+     <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center gap-8">
+        {/* Logo and Title */}
+        <Link href = "/">
+            <h1 className="text-4xl font-semibold">
+                Hemanshu<span className="text-accent">.</span>
+            </h1>
+        </Link>
+
+        {/* desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href = "/contact">
+                <Button>Hire Me</Button>
+            </Link>
+        </div>
+
+        {/* Mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+
+    </div>
+  </header>
+    );
+};
+
+export default header
