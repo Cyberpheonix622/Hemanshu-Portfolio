b9bc57dcecb80582a3aaed2bb2681867c37f13c1
Creepboi <cyberpheonix622@gmail.com>
2025-07-19 19:14:20 +0530

    Basic intrface for mobile navigation and sheet components need to do buttons now

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import {Sheet, SheetContent, SheetTrigger, SheetTitle} from "@/components/ui/sheet";
 import {usePathname} from 'next/navigation';
 import Link from "next/link";
 import {CiMenuFries} from "react-icons/ci";
@@ -37,9 +37,27 @@ const MobileNav = () => {
         <CiMenuFries className="text-32px text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        <div>
-          Logo
-        </div>
+        <SheetTitle className="text-white">
+          <div className="mt-32 mb-40 text-center text-2xl">
+            <Link href="/">
+              <h1 className="text-4xl font-semibold">
+                Hemanshu<span className="text-accent">.</span></h1>
+            </Link>
+          </div>
+          <nav className="flex flex-col justify-center items-center gap-8">
+            {links.map((link, index) => {
+              return(
+                <Link 
+                  href={link.path}
+                  key={index} 
+                  className={'${link.path === pathname && "text-accent border-b-2 border-accent "}text-xl capitalize hover:text-accent transition-all'}
+                >
+                  {link.name}
+                </Link>
+              );
+            })}
+          </nav>
+        </SheetTitle>
       </SheetContent>
     </Sheet>
   );
