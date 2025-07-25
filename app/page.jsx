ea5b95d67af3b398f4f1422ef7aa5b7d4f7eb333
Creepboi <cyberpheonix622@gmail.com>
2025-07-25 23:35:48 +0530

    photo added

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
-            <span className="text-xl">Apiring Ethical Hacker</span>
+          <div className="text-center xl:text-left order-4 xl:order-none">
+            <span className="text-xl">Aspiring Ethical Hacker</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /><span className="text-accent">Hemanshu Moundekar</span>
             </h1>
@@ -37,7 +37,7 @@ const Home = () => {
             </div>
             
           </div>
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
