d5877037bca67996e4807c33500b37d3b5cb710d
Creepboi <cyberpheonix622@gmail.com>
2025-07-27 01:28:55 +0530

    Contact page connection remaining

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -50,15 +50,37 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px] ">
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">For any query or to reach out</h3>
+              <h3 className="text-4xl text-accent">To Reach Out</h3>
               <p>
-                
+               To reach me out please contact me through the below form.
               </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+                <Input type="firstname" placeholder="First Name"/>
+                <Input type="lastname" placeholder="Last Name"/>
+                <Input type="email" placeholder="Email Address"/>
+                <Input type="phone" placeholder="Phone No"/>
+              </div>
+              <Textarea className="h-[200px]" placeholder= "Type Your Message Here" />
+              <Button size="md" className="max-w-40">
+                Send Message
+              </Button>
             </form>
           </div>
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8
           xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return <li key={index} className="flex items-center gap-6 ">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              })}
+            </ul>
           </div>
         </div>
       </div>
