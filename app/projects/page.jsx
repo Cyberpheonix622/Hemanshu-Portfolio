0672daef1e330eedea6ebb51c05e2316f020967b
Creepboi <cyberpheonix622@gmail.com>
2025-07-30 10:18:32 +0530

    Added the projects now UI remaining

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -27,8 +27,36 @@ const project = [
     title: 'Java Based Simple keylogger',
     description: 'A simple Java-based keylogger for educational and ethical use only. Captures and logs keystrokes to a local file. Intended for learning purposes in cybersecurity and ethical hacking. Do not use on unauthorized devices.',
     stack: [{name:'Java'}, {library:'JNativeHook'}],
-    image: '/assets/'
-  }
+    image: '/assets/Projects/image1.png',
+    github: "https://github.com/Cyberpheonix622/Keylogger"
+  },
+  {
+    num: '02',
+    category: "Backend",
+    title: 'Python based Simple Packet Sniffer',
+    description: ' Python Packet Sniffer A low-level network packet sniffer written in Python using raw sockets. This tool captures Ethernet frames and decodes IPv4/IPv6, TCP, UDP, ICMP, and ICMPv6 protocols in real-time.',
+    stack: [{name:'Python'}, {library:'Scapy'},{library:'Socket'}, {technologies: 'Npcap'}],
+    image: '/assets/Projects/image2.png',
+    github: "https://github.com/Cyberpheonix622/Sniffer"
+  },
+    {
+    num: '03',
+    category: "Full Stack",
+    title: 'PhishFinder',
+    description: 'PhishFinder is a lightweight and intelligent phishing detection tool designed to help users identify and avoid malicious URLs or websites. Leveraging machine learning and domain analysis techniques, PhishFinder classifies web links as legitimate or phishing, helping prevent credential theft, financial fraud, and cyberattacks.',
+    stack: [{name:'Python'}, {name:'React+vite'}, {library:'Scikit-learn'}, {library:'Pandas'}, {library:'NumPy'}, {library:'Flask'}],
+    image: '/assets/Projects/image3.png',
+    github: "https://github.com/Cyberpheonix622/PhishFinder"
+  },
+    {
+    num: '04',
+    category: "Full Stack",
+    title: 'SecLog',
+    description: 'Seclog is a Python-based GUI tool for analyzing Windows Event Logs in real time. It includes role-based login, search, filtering, visualization, export features, and is packaged for standalone use with PyInstaller and Inno Setup.',
+    stack: [{name:'Python'}, {library:'ttkbootstrap'}, {library:'pywin32'}, {library:'matplolib'}],
+    image: '/assets/Projects/image4.png',
+    github: "https://github.com/Cyberpheonix622/Seclog"
+  },
 ]
 
 const Projects = () => {
