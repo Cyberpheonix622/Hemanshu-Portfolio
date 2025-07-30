b62e97bf47ca883951bb87d935f77ccc5ce5d75c
Creepboi <cyberpheonix622@gmail.com>
2025-07-30 20:49:33 +0530

    fixed mobile rendering issue

---
+++ app/head.jsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Portfolio</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="UTF-8" />
+    </>
+  );
+}
